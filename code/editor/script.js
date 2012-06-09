48096ffcc1573d98e6a22224674baa17e9c05d82
Christoph Hochreiner <ch.hochreiner@gmail.com>
2012-06-09 22:01:15 +0200

    added mocked interaction

--- code/editor/script.js
+++ code/editor/script.js
@@ -15,45 +15,22 @@ function changeScript() {
   editor.gotoLine(1);
 }
 
-
-function loadSamples(name) {
-var xmlhttp;
-if (window.XMLHttpRequest) {
-    xmlhttp=new XMLHttpRequest();
-  } else{
-    xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+function submitScript() {
+  if (Math.round(Math.random())) {
+    document.getElementById("result").innerHTML="result"
+  } else {
+    errorMessage= " Commands:\n"
+    + "invalid command name \"fore\"\n"
+    + "while executing\n"
+    + "fore ach {i} [info functions] {\n"
+    + "  if {![info exists availablecommands]} {\n"
+    + "    set availablecommands $i\n"
+    + "  } else {\n"
+    + "     set availablecommands \"$availabl...\"\n"
+    + " (file \"generator.tcl\" line 13)";
+    var line = errorMessage.match(/line (.*)\)$/);
+    editor.gotoLine(line[1]);
+    document.getElementById("result").innerHTML=errorMessage;    
   }
-  xmlhttp.onreadystatechange=function() {
-  if (xmlhttp.readyState==4 && xmlhttp.status==200) {
-    editor.setValue("moep");
-    //document.getElementById("myDiv").innerHTML=xmlhttp.responseText;
-    }
-  }
-  xmlhttp.open("GET",url,true);
-  xmlhttp.send();
-}  
-
-function loadXMLDoc(url, option) {
-var xmlhttp;
-if (window.XMLHttpRequest) {
-    xmlhttp=new XMLHttpRequest();
-  } else{
-    xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
-  }
-  xmlhttp.onreadystatechange=function() {
-  if (xmlhttp.readyState==4 && xmlhttp.status==200) {
-    editor.setValue("moep");
-    //document.getElementById("myDiv").innerHTML=xmlhttp.responseText;
-    }
-  }
-  xmlhttp.open("GET",url,true);
-  xmlhttp.send();
 }
-
-
-
-
-
-
-
 
