bc7ea0ef92095f473789ec70bdb82719b99dd43e
Christoph Hochreiner <ch.hochreiner@gmail.com>
2013-05-22 22:22:58 +0200

    object tests

--- code/evaluator/evaluator.tcl
+++ code/evaluator/evaluator.tcl
@@ -1,18 +1,25 @@
-package require XOTcl
-namespace import -force ::xotcl::*
+package require nx
+namespace import -force ::nx::*
 
 
 # definitons to test the script
-set ::story "Given there exists a procedure test2.|Given there exists a procedure test3 with the parameter asdf.
-\Given that the variable asdf is assigned to the value -1.
-\When the procedure test is called, 1 is returned.
-\When the procedure test is called, 2 is returned&Given there exists an object objectC of the type qwert.
-\Given there exists an object objectE | Given there exists an object objectC.
-\Given there exists an object objectB."
+set ::story "Given there exists an object objectA of the type C."
+#\Given there exists a procedure test2.|Given there exists a procedure test3 with the parameter asdf.
+#\Given that the variable asdf is assigned to the value -1.
+#\When the procedure test is called, 1 is returned.
+#\When the procedure test is called, 2 is returned&Given there exists an object objectC of the type qwert.
+#\Given there exists an object objectE | Given there exists an object objectC.
+#\Given there exists an object objectB."
+
+
+
 
 set ::submittedCode "proc test {} {return 1}
 \proc test1 {asdf} {return \$asdf}
-\set asdf -1"
+\set asdf -1
+\Class create C {}
+\C create objectA"
+
 
 #Configuration
 #strictStory = only evaluate the next sentence, iff the current sentence is valid
@@ -52,7 +59,6 @@ proc orCase {sentence} {
   }
 }
 
-
 proc evaluate {sentence} {
   set sentenceToEvaluate [string trim $sentence]
 # package require does not work in safe mode
@@ -63,10 +69,7 @@ proc evaluate {sentence} {
   append script "package require nx \n namespace import -force ::nx::* \n "
   append script "set auditVariable \"\" \n"
 
-  
   append script "\n $::submittedCode \n"
-
-  
   
   if {[catch {set result [interp eval $i $script]} msg x]} {
     set result "Errormessage: $msg \n\n"
@@ -75,6 +78,29 @@ proc evaluate {sentence} {
     append ::overallFeedback "The provided code is not executable: \n $result \n"
     return "The provided code is not executable \n"
   }
+ 
+
+   if {[regexp {Given there exists an object (.+) of the type (.+)} $sentenceToEvaluate _ param1 param2]} {
+    append script "$param1 info has type $param2"
+
+    catch {set result [interp eval $i $script]} msg x
+    if {$result == "0" || [regexp {expected class but got (.+)} $msg _ _] } {
+      append ::overallFeedback "Failed: $sentence \n"
+      return "Failed: $sentence \n"  
+    }
+    return
+  }  
+ 
+  if {[regexp {Given there exists an object (.+)} $sentenceToEvaluate _ param1]} {
+    append script "::nsf::object::exists $param1 \n"
+ 
+    set result [interp eval $i $script]
+    if {$result == "0"} {
+      append ::overallFeedback "Failed: $sentence \n"
+      return "Failed: $sentence \n"  
+    }
+    return
+  }  
  
   if {[regexp {Given there exists a procedure (.+) with the parameter (.+)} $sentenceToEvaluate _ param1 param2]} {
   append script "set x \[$param1 $param2]"
@@ -101,7 +127,7 @@ proc evaluate {sentence} {
   }
   
   if {[regexp {Given that the variable (.+) is assigned to the value (.+)} $sentenceToEvaluate _ param1 param2]} {
-    append script "if {$$param1 != $param2} {append auditVariable $$param1}"
+    append script "if {$$param1 != $param2} {append auditVariable \"failed\"}"
     append script "\n return \$auditVariable"
  
  
@@ -111,6 +137,7 @@ proc evaluate {sentence} {
       append ::overallFeedback "Failed: $sentence \n"
       return "Failed: $sentence \n"  
     }
+    return
   }  
   
   if {[regexp {When the procedure (.+) is called, (.+) is returned} $sentenceToEvaluate _ param1 param2]} {
@@ -123,6 +150,7 @@ proc evaluate {sentence} {
       append ::overallFeedback "Failed: $sentence \n"
       return "Failed: $sentence \n"  
     }
+    return
   }
   
   if {[regexp {When the procedure (.+) is called, the program does not terminate.} $sentenceToEvaluate _ param1]} {
@@ -142,7 +170,6 @@ proc evaluate {sentence} {
  
 # Structural:
 
-#* Given there exists an object *concreteObjectName* [of the type *className*].
 #* Given there exists a variable *variableName* in the object/class *concreteInstanceName/className*. 
 #* Given there exists a procedure *procedureName* [for the class/object *className/concreteInstanceName*].
 #* Given there exists a class *classname* [that takes one parameter].
